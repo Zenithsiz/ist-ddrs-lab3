d2f7cc262f098add2c450540a6ec124056bd5454
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2023-12-20 21:49:32 +0000

    Improved 1.

--- code/1.R
+++ code/1.R
@@ -1,26 +1,6 @@
 require(lpSolve)
 
-# Flows:
-# 1 -> 4 (4.5 MiB/s)
-# 2 -> 4 (2.5 MiB/s)
-# 2 -> 3 (4.5 MiB/s)
-# 1 -> 3 (1.5 MiB/s)
-
-# Routes
-# 1 -> 4:
-#   1 -> 2 -> 4
-#   1 -> 2 -> 3 -> 4
-#   1 -> 3 -> 4
-# 2 -> 4:
-#   2 -> 4
-#   2 -> 3 -> 4
-# 2 -> 3:
-#   2 -> 3
-# 1 -> 3:
-#   1 -> 3
-#   1 -> 2 -> 3
-
-# Order:
+# Routes:
 idx_x_124 <- 1
 idx_x_1234 <- 2
 idx_x_134 <- 3
@@ -29,16 +9,20 @@ idx_x_234 <- 5
 idx_x_23 <- 6
 idx_x_13 <- 7
 idx_x_123 <- 8
-idx_r <- 9
 
-# Flow amounts
+# Flows:
 b_14 <- 4.5
 b_24 <- 2.5
 b_23 <- 4.5
 b_13 <- 1.5
 
 # Capacity
-c <- 10
+c_total <- 10
+
+# Objective: `r`
+idx_r <- 9
+objective <- replicate(9, 0)
+objective[idx_r] <- 1
 
 # Equations:
 # (Link 1 -> 2): b_14 * x_124  + b_14 * x_1234 + b_13 * x_123              - c * r <= 0
@@ -50,10 +34,6 @@ c <- 10
 # (Flow 2): x_24 + x_234 = 1
 # (Flow 3): x_23 = 1
 # (Flow 4): x_13 + x_123 = 1
-
-objective <- replicate(9, 0)
-objective[idx_r] <- 1
-
 mat <- matrix(
   0,
   nrow = 9,
@@ -63,23 +43,23 @@ mat <- matrix(
 dir <- replicate(9, "")
 rhs <- replicate(9, 0)
 
-mat[1, c(idx_x_124, idx_x_1234, idx_x_123, idx_r)] <- c(b_14, b_14, b_13, -c)
+mat[1, c(idx_x_124, idx_x_1234, idx_x_123, idx_r)] <- c(b_14, b_14, b_13, -c_total)
 dir[1] <- "<="
 rhs[1] <- 0
 
-mat[2, c(idx_x_134, idx_x_13, idx_r)] <- c(b_14, b_13, -c)
+mat[2, c(idx_x_134, idx_x_13, idx_r)] <- c(b_14, b_13, -c_total)
 dir[2] <- "<="
 rhs[2] <- 0
 
-mat[3, c(idx_x_1234, idx_x_234, idx_x_23, idx_x_123, idx_r)] <- c(b_14, b_24, b_23, b_13, -c)
+mat[3, c(idx_x_1234, idx_x_234, idx_x_23, idx_x_123, idx_r)] <- c(b_14, b_24, b_23, b_13, -c_total)
 dir[3] <- "<="
 rhs[3] <- 0
 
-mat[4, c(idx_x_124, idx_x_24, idx_r)] <- c(b_14, b_24, -c)
+mat[4, c(idx_x_124, idx_x_24, idx_r)] <- c(b_14, b_24, -c_total)
 dir[4] <- "<="
 rhs[4] <- 0
 
-mat[5, c(idx_x_1234, idx_x_134, idx_x_234, idx_r)] <- c(b_14, b_14, b_24, -c)
+mat[5, c(idx_x_1234, idx_x_134, idx_x_234, idx_r)] <- c(b_14, b_14, b_24, -c_total)
 dir[5] <- "<="
 rhs[5] <- 0
 
@@ -99,10 +79,8 @@ mat[9, c(idx_x_13, idx_x_123)] <- c(1, 1)
 dir[9] <- "=="
 rhs[9] <- 1
 
-print(mat)
-
-
-bin <- c(
+bin_a <- c()
+bin_b <- c(
   idx_x_124,
   idx_x_1234,
   idx_x_134,
@@ -113,7 +91,7 @@ bin <- c(
   idx_x_123
 )
 
-optimum <- lp(
+optimums <- lapply(list(bin_a, bin_b), \(bin) lp(
   direction = "min",
   objective.in = objective,
   const.mat = mat,
@@ -122,9 +100,14 @@ optimum <- lp(
   # Without bifurcation, x_* must be binary (0 or 1 only), using `binary.vec`
   # With bifurcation, `binary_vec` must be empty
   binary.vec = bin
-)
-print(optimum$solution)
-print(optimum$objval)
+))
+optimum_a <- optimums[[1]]
+optimum_b <- optimums[[2]]
 
-# Solution: 0.44444, 0.55555, 0.0, 0.0, 1.0, 1.0, 0.0, 0.45
-#           0.45
+cat("Solution a:\n")
+print(optimum_a$solution)
+print(optimum_a$objval)
+
+cat("Solution b:\n")
+print(optimum_b$solution)
+print(optimum_b$objval)
