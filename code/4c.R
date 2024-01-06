d8a2dfbc1da7f0c4212664b9aafb1b0ea5cda584
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2024-01-06 10:03:40 +0000

    Started ex 4c.

--- code/4c.R
+++ code/4c.R
@@ -10,9 +10,34 @@ numNodes <- nrow(R) # Total number of nodes
 numODPairs <- numNodes^2 - numNodes # Total number of OD Pairs
 numRoutes <- numODPairs # Total number of routes
 
-Mu <- 1e9 / 8e3 # Service rate in packets/second
+prev_best_rates <- matrix(
+  c(
+    c(0, 15000, 0, 0, 15000, 0, 0, 0, 0, 0, 3000, 0, 0, 0, 0, 0, 0),
+    c(6750, 0, 82000, 0, 42000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+    c(0, 80000, 0, 18000, 0, 106500, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+    c(0, 0, 11000, 0, 24500, 54750, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+    c(21000, 44000, 0, 21000, 0, 0, 68000, 0, 0, 0, 44000, 0, 0, 0, 0, 0, 0),
+    c(0, 0, 105000, 30750, 0, 0, 27000, 0, 89250, 0, 0, 108625, 0, 0, 0, 0, 0),
+    c(0, 0, 0, 0, 60000, 19500, 0, 61250, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+    c(0, 0, 0, 0, 0, 0, 43750, 0, 42250, 0, 15750, 0, 41250, 0, 0, 0, 0),
+    c(0, 0, 0, 0, 0, 96250, 0, 52000, 0, 45500, 0, 0, 0, 0, 0, 0, 0),
+    c(0, 0, 0, 0, 0, 0, 0, 0, 48750, 0, 0, 29750, 0, 0, 19250, 0, 0),
+    c(2750, 0, 0, 0, 48000, 0, 0, 15000, 0, 0, 0, 0, 0, 57750, 0, 0, 0),
+    c(0, 0, 0, 0, 0, 81125, 0, 0, 0, 27625, 0, 0, 0, 0, 0, 34000, 0),
+    c(0, 0, 0, 0, 0, 0, 0, 46000, 0, 0, 0, 0, 0, 83875, 43125, 0, 0),
+    c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 78750, 0, 75000, 0, 0, 0, 0),
+    c(0, 0, 0, 0, 0, 0, 0, 0, 0, 19250, 0, 0, 43125, 0, 0, 0, 24500),
+    c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 74000, 0, 0, 0, 0, 12000),
+    c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 24500, 15000, 0)
+  ),
+  nrow = 17,
+  ncol = 17,
+)
 
-set.seed(0)
+extra_capacity <- 0.97434 * prev_best_rates / max(prev_best_rates)
+
+Mu <- (1 + extra_capacity) * 1e9 / 8e3
+
 GlobalBest <- Inf
 for (iter in 1:numIterations) {
   CurrentSolution <- GreedyRandomized(R, Tr, L, Mu)
@@ -61,17 +86,3 @@ GlobalBestAll <- Evaluate(GlobalBestSolution, Tr, R, Mu)
 
 cat(sprintf("Maximum link load = %f\n", GlobalBestAll[2]))
 cat(sprintf("Average packet delay = %f\n", GlobalBestAll[1]))
-cat("Best solution:\n")
-print(GlobalBestSolution)
-
-λ <- matrix(0, nrow = numNodes, ncol = numNodes)
-for (routes in GlobalBestSolution) {
-  for (idx in 2:length(routes)) {
-    node_src <- routes[[idx - 1]]
-    node_dst <- routes[[idx]]
-    λ[node_src, node_dst] <- λ[node_src, node_dst] + Tr[node_src, node_dst] * 1e6 / 8e3
-  }
-}
-
-cat("Best solution rates:\n")
-print(λ)
