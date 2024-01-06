b0a2a1b5a1771fb40e5df6013dba6916cbeac5f7
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2024-01-06 09:24:09 +0000

    Fixed 4b.

--- code/util/greedy_randomized.R
+++ code/util/greedy_randomized.R
@@ -1,30 +1,24 @@
-GreedyRandomized <- function(nodes, traffic, link_lengths, mus) {
+GreedyRandomized <- function(nodes, traffic, link_lengths, μ) {
   # Find all flows and shuffle them
   nodes_len <- nrow(nodes)
   flows <- lapply(1:nodes_len, \(i) lapply((1:nodes_len)[-i], \(j) list(src = i, dst = j)))
   flows <- unlist(flows, recursive = FALSE)
   flows <- sample(flows, length(flows))
 
-  # Calculates the link delays
-  rates <- matrix(0, nrow = nodes_len, ncol = nodes_len)
-  calc_link_delays <- function() {
-    # Calculate the in-system delays
-    sys_delays <- 1 / (mus - rates)
-    sys_delays[sys_delays <= 0] <- Inf
-    sys_delays[is.nan(sys_delays)] <- Inf
+  # Calculates the transmission link delays
+  # Note: If no edge exists between two nodes, we set their transmission link delay to `Inf`.
+  λ <- matrix(0, nrow = nodes_len, ncol = nodes_len)
+  trans_delays <- link_lengths * 1e3 / 3e8
+  trans_delays[!nodes] <- Inf
 
-    # Then add the propagation delay (converted to seconds)
-    link_delays <- nodes * (sys_delays + link_lengths * 1e3 / 3e8)
-    link_delays[is.nan(link_delays)] <- Inf
+  # Calculate the shortest path between all flows
+  flow_routes <- lapply(flows, function(flow) {
+    # Calculate the current link delays from the transmission link delays and the in-system delays
+    sys_delays <- 1 / (μ - λ)
+    link_delays <- sys_delays + trans_delays
 
-    link_delays
-  }
-
-  # Calculate the shortest path between all pairs
-  lapply(flows, function(flow) {
-    # Calculate the current link delays and find the best route for this flow
+    # And find the best route for this flow considering the link delays
     # Note: `routes` contains the cost at the end, so we remove it.
-    link_delays <- calc_link_delays()
     routes <- ShortestPath(link_delays, flow$src, flow$dst)
     routes <- routes[-length(routes)]
 
@@ -32,10 +26,18 @@ GreedyRandomized <- function(nodes, traffic, link_lengths, mus) {
     for (idx in 2:length(routes)) {
       node_src <- routes[[idx - 1]]
       node_dst <- routes[[idx]]
-      rates[node_src, node_dst] <<- rates[node_src, node_dst] + traffic[node_src, node_dst] * 1e6 / 8e3
+      λ[node_src, node_dst] <<- λ[node_src, node_dst] + traffic[node_src, node_dst] * 1e6 / 8e3
     }
 
     # And return the route
     routes
   })
+
+  # If we broke our invariant, retry
+  if (any(λ >= μ)) {
+    cat("GreedyRandomized: Invariant broken, trying another solution\n")
+    return(GreedyRandomized(nodes, traffic, link_lengths, μ))
+  }
+
+  flow_routes
 }
